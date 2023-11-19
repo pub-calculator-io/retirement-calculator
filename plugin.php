532378f31ff99a9648e935b9d02809461d32a464
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:30:03 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Retirement Calculator by Calculator.iO
+Plugin Name: Retirement Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/retirement-calculator/
 Description: Retirement savings calculator that uses the formula FV = PV (1 + R)^n to help users plan for retirement and calculate retirement income.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Retirement Cal
 
 function display_ci_retirement_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/retirement-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Retirement Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_retirement_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Retirement Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_retirement_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_retirement_calculator', 'display_ci_retirement_calculator' );
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ Retirement savings calculator that uses the formula FV = PV (1 + R)^n to help us
 
 ## Installation
 
-1. [Download](https://github.com/pub-calculator-io/age-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
+1. [Download](https://github.com/pub-calculator-io/retirement-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
 2. Upload the /retirement-calculator-master/ folder to the /wp-content/plugins/ directory.
 3. Activate the [Retirement Calculator](https://www.calculator.io/retirement-calculator/ "Retirement Calculator Homepage") plugin through the "Plugins" menu in WordPress.
 
